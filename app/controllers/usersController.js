fe3f7828c56f880eb748f14f576ef9a54f641f94
pratheekmurthy <pratheekkmurthy516@gmail.com>
2021-05-02 23:28:17 +0530

    login check

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,4 +1,6 @@
+const { findOne } = require('../models/user')
 const User = require('../models/user')
+const bycrpt = require('bcryptjs')
 const usersController ={}
 
 usersController.register =(req,res)=>{
@@ -14,6 +16,30 @@ usersController.register =(req,res)=>{
 }
 
 usersController.login =(req,res)=>{
+    const body = req.body
+    User.findOne({email : body.email})
+    .then((user)=>{
+        if(user){
+            //res.json(user)
+            bycrpt.compare(body.password,user.password)
+                .then((result)=>{
+                    // res.send(result)
+                    if(result){
+                        res.send("email password matches")
+                    }else {
+                        res.send('password does not match')
+                    }
+                })
+                .catch((err)=>{
+                    res.json(err)
+                })
+        }else{
+            res.json({error : 'Invalid email /password'})
+        }
+    })
+    .catch((err)=>{
+        res.json(err)
+    })
 
 }
 
